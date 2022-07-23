77fb95582257154db6032bcf5759465e1adc0b16
jake dapper <jakedapper@gmail.com>
2022-07-23 12:58:12 -0500

    commit tree 5168ea606bcadad644186ca0a04ea613309bf259

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching (driverArray, name){
+    return driverArray.filter((driver) => driver.toLowerCase() === name.toLowerCase())
+}
+
+
+function fuzzyMatch (driverArray, name){
+    return driverArray.filter((driver) => driver[0].toLowerCase() === name[0].toLowerCase())
+}
+
+function matchName (driverObjectArray, names) {
+    return driverObjectArray.filter((driver)=> driver.name === names)
+}
